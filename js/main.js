45e090f24c60ce0da0e42a2a81721d94f7423672
Kely Vargaya <kelyvargayan@gmail.com>
2021-09-28 11:43:36 -0500

    Resultados archivos

--- js/main.js
+++ js/main.js
@@ -92,6 +92,38 @@
     TweenMax.to( $heroB, 1, { rotationY: -0.025 * sxPos, rotationX: -0.02 * syPos, rotationZ: 0, transformPerspective:500, transformOrigin:'center center' });
 	});
   
+
+	let i=2;
+
+	
+		var radius = 200;
+		var fields = $('.itemDot');
+		var container = $('.dotCircle');
+		var width = container.width();
+ radius = width/2.5;
+ 
+		 var height = container.height();
+		var angle = 0, step = (2*Math.PI) / fields.length;
+		fields.each(function() {
+			var x = Math.round(width/2 + radius * Math.cos(angle) - $(this).width()/2);
+			var y = Math.round(height/2 + radius * Math.sin(angle) - $(this).height()/2);
+			if(window.console) {
+				console.log($(this).text(), x, y);
+			}
+			
+			$(this).css({
+				left: x + 'px',
+				top: y + 'px'
+			});
+			angle += step;
+		});
+		
+
+		var header = $('.marca-planeta-4');
+    setTimeout(function() {
+      header.addClass('planeta-active');
+    }, 15000);
+
   })(jQuery);
 
   /* ----- Control video ----- */
